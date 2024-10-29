3735907973cece527291be7a9d2f21797ecc6c7b
Jonas Stocklasser <jonas.stocklasser@gmail.com>
2024-10-29 17:13:39 +0100

    1029202404

--- Assets/Scripts/Package/TestRun01.py
+++ Assets/Scripts/Package/TestRun01.py
@@ -139,7 +139,7 @@ class TestRun01(ctk.CTkFrame):  # class for the TestRun01 window
 
         temperature = self.get_temperature_w1()
         pressure = ina.current()
-        print(f"pressure-amp: {pressure}mA")
+        print(f"pressure-amp: {pressure}mA\npressure-volt: {ina.voltage()}V\npressure-power: {ina.power()}W")
 
         pressure_values.append(pressure)
         temperature_values.append(temperature)

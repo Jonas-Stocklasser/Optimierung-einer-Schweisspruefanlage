ebcfd3035b2c33c90c11f6be3178d379a5d08629
Jonas Stocklasser <jonas.stocklasser@gmail.com>
2024-10-31 11:13:12 +0100

    1031202401

--- Assets/Scripts/Package/TestRun01.py
+++ Assets/Scripts/Package/TestRun01.py
@@ -24,11 +24,11 @@ GPIO.output(14, False)
 output = 0
 
 ina = INA219(shunt_ohms=0.1,
-             max_expected_amps = 0.03,
+             max_expected_amps = 0.6,
              address=0x40,
              busnum=1)
 
-ina.configure(voltage_range=ina.RANGE_32V,
+ina.configure(voltage_range=ina.RANGE_16V,
               gain=ina.GAIN_AUTO,
               bus_adc=ina.ADC_128SAMP,
               shunt_adc=ina.ADC_128SAMP)

62388a31bb498986235ae580a4ad56faae2090aa
Jonas Stocklasser <jonas.stocklasser@gmail.com>
2025-02-05 11:17:55 +0100

    02052025

--- Assets/Scripts/Package/StartScreen.py
+++ Assets/Scripts/Package/StartScreen.py
@@ -98,6 +98,5 @@ class StartScreen(ctk.CTkFrame):  # class for the StartScreen window
 
     def new_test_button_function(self, which):
         json_writer("startup_var", "firstControlStartup", 1, main_pi_location + "../JSON/")
-        self.master.switch_window("1.0")
-        self.master.NewTestScreen02.reset_input_new_Test()
-
+        self.master.switch_windows(which)
+        self.master.windows.get("1.1").reset_input_new_test()

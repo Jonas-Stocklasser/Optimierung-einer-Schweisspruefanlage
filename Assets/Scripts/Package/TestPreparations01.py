8c9f9e3c7b352d9393a0ce2e55ed68b5272cd30a
Jonas Stocklasser <jonas.stocklasser@gmail.com>
2025-02-05 10:29:51 +0100

    02052025

--- Assets/Scripts/Package/TestPreparations01.py
+++ Assets/Scripts/Package/TestPreparations01.py
@@ -96,13 +96,13 @@ class TestPreparations01(ctk.CTkFrame):  # class for the TestPreparations01 wind
 
         self.instruction_label = ctk.CTkLabel(master=self.text_frame,
                                               anchor="nw",
-                                              text="Anweisungen des ausgedruckten\nDokuments befolgen\n"
+                                              text="Anweisungen des ausgedruckten\nDokuments befolgen!\n\n"
                                                    "1. Prüfstück mit Wasser füllen\n"
                                                    "2. Flansch auf Prüfstück aufsetzen\n"
                                                    "3. Prüfstück am Flansch aufhängen\n"
                                                    "4. Prüfstück entlüften\n"
                                                    "5. Prüfstück in das Becken hinablassen\n"
-                                                   "6. Prüfvorgang starten (erst nach Entlüftung!)\n"
+                                                   "6. Prüfvorgang starten (erst nach Entlüftung!)\n    -> Weiter drücken"
                                                    "\n"
                                                    "-----------------------------------------------------------\n"
                                                    "Pumpe toggeln - ENTER\n"

ca4972b53b44dcf30890659cc68ad6639f96e7c7
Jonas Stocklasser <jonas.stocklasser@gmail.com>
2025-02-16 10:41:52 +0100

    02162025

--- Assets/Scripts/Package/TestRun01.py
+++ Assets/Scripts/Package/TestRun01.py
@@ -667,8 +667,6 @@ class TestRun01(ctk.CTkFrame):  # class for the TestRun01 window
         pdf.ln(5)
         pdf.ln(5)
         pdf.ln(5)
-        pdf.ln(5)
-        pdf.ln(5)
         if passed == 1:
             grade[0] = 1
         elif passed == 0:
